b147d3067e3e12f9374f09c54ea10d03ebf06892
srihari38 <srihariprasad993@gmail.com>
2018-12-10 22:16:34 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from hell import create_app
-
+import os
+port_1 = int(os.environ["PORT"])
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port_1, debug=True)
